7f00babcd722a197fd2c01200dfd09e1c285a393
Jhonata Tenório <51134324+JhN-Te@users.noreply.github.com>
2020-05-18 12:20:42 -0300

    Add files via upload

---
+++ Atribuicao.js
@@ -0,0 +1,14 @@
+const a = 2;
+let b = 4;
+// '=' Não é comparação, e sim atribuição!
+
+b += a; // b + a;
+console.log(b);
+b -= a; // b - a;
+console.log(b);
+b *= a; // b * a;
+console.log(b);
+b /= a; // b / a;
+console.log(b);
+b %= a; // O resto da divisão de b por a;
+console.log(b);
